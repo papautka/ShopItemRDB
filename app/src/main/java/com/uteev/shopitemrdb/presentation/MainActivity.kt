65826ea793ee159f4b68ac775b6e31589716baa3
andrew <120500uas@gmail.com>
2024-04-25 16:26:09 +0300

    Initial commit

---
+++ app/src/main/java/com/uteev/shopitemrdb/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.uteev.shopitemrdb.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.uteev.shopitemrdb.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
